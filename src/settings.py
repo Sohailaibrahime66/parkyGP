e65ed675727bfbc9707d8ed071eabd6c882b5ff9
Sohailaibrahime66 <124284142+Sohailaibrahime66@users.noreply.github.com>
2025-04-19 01:54:56 +0200

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -79,6 +79,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -87,7 +88,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
 
 ]
 CORS_ALLOW_CREDENTIALS = True
