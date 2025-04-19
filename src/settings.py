3c63ef8753a6db6bb8e08c20a8d9c8b3b721ca44
Sohailaibrahime66 <124284142+Sohailaibrahime66@users.noreply.github.com>
2025-04-19 15:58:58 +0200

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -142,10 +142,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'src.wsgi.application'
 
-AUTHENTICATION_BACKENDS = [
-    'parking.authentication.EmailBackend',
-    'django.contrib.auth.backends.ModelBackend',  # optional fallback
-]
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
