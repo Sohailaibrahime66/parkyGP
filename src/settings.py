9b42d95caae127448fba228ff945134b4c6e52ed
Sohailaibrahime66 <124284142+Sohailaibrahime66@users.noreply.github.com>
2025-04-19 13:46:36 +0200

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -95,7 +95,7 @@ CORS_ALLOW_CREDENTIALS = True
 #CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "https://parky-dashboard.vercel.app",
-    "*"," http://localhost:5173/", "http://127.0.0.1:5173/",
+    " http://localhost:5173", "http://127.0.0.1:5173",
 ]
 
 
