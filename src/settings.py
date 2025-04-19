d9a0a1edfe648d2165ecc0e8268bc1400f439e63
Sohailaibrahime66 <124284142+Sohailaibrahime66@users.noreply.github.com>
2025-04-19 02:11:24 +0200

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -92,12 +92,30 @@ MIDDLEWARE = [
 
 ]
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_ALL_ORIGINS = True
+#CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "https://parky-dashboard.vercel.app",
     "*"," http://localhost:5173/", "http://127.0.0.1:5173/",
 ]
 
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "authorization",
+    "content-type",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
 ROOT_URLCONF = 'src.urls'
 
 TEMPLATES = [
